89df7f518ed0e02287ea71c1e33c9becf23552df
Mursalin <mursalin2311@gmail.com>
2025-02-21 23:40:30 +0600

    installed react router dom now will work on the flow and plan chart of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="flex justify-center h-screen items-center text-5xl text-red-400">
+    <div className="flex justify-center h-screen items-center text-5xl text-red-400 bg-black">
       Hello New Project
     </div>
   );
